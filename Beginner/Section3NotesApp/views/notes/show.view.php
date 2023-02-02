ec85c78ac0ae01afe504d0d9f46a83f8938db48a
Muhammad Junaid Khan <junaid.khan@codenterprise.com>
2023-02-02 15:18:18 +0500

    Episode 29 Resourcefull naming Conventions

--- Beginner/Section3NotesApp/views/notes/show.view.php
+++ Beginner/Section3NotesApp/views/notes/show.view.php
@@ -1,6 +1,6 @@
-<?php require('partials/head.php') ?>
-<?php require('partials/nav.php') ?>
-<?php require('partials/banner.php') ?>
+<?php require('views/partials/head.php') ?>
+<?php require('views/partials/nav.php') ?>
+<?php require('views/partials/banner.php') ?>
 
 
 
@@ -20,4 +20,4 @@
         <!-- /End replace -->
     </div>
 </main>
-<?php require('partials/footer.php') ?>
+<?php require('views/partials/footer.php') ?>
