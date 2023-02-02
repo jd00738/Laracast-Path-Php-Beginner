4fe4a6562fc00a1c80fb11e2c4c7213d514fc42f
Muhammad Junaid Khan <junaid.khan@codenterprise.com>
2023-02-02 14:20:29 +0500

    Episode 27 form validation

--- Beginner/Section3NotesApp/controllers/note-create.php
+++ Beginner/Section3NotesApp/controllers/note-create.php
@@ -9,9 +9,25 @@ $db = new Database($config['database']);
 $heading = "New Notes";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $db->query('INSERT INTO notes (body,user_id) Values(:body,:userId)', [
-        "body" => $_POST['body'],
-        "userId" => 2
-    ]);
+
+    $error = [];
+    if (strlen($_POST['body']) == 0) {
+        $error['body'] = "Body is required";
+    }
+
+    if (strlen($_POST['body']) < 10) {
+        $error['body'] = "minimum allowed character are 10";
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $error['body'] = "maximum allowed character are 1000";
+    }
+
+    if (empty($error)) {
+        $db->query('INSERT INTO notes (body,user_id) Values(:body,:userId)', [
+            "body" => $_POST['body'],
+            "userId" => 2
+        ]);
+    }
 }
 require "views/note-create.view.php";
